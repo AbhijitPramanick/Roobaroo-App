a5e3c0996af2f853a8ed7d3e4bbee4b98bbfe28a
AbhijitPramanick <amanik1995@gmail.com>
2024-01-09 15:28:29 +0530

    Delete button for post enabled.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,25 +5,29 @@ import React, { useState, useEffect } from "react";
 const HomePage = ({ userIdData }) => {
   const [user, setUser] = useState({});
   const [userPosts, setUserPosts] = useState([]);
-
+  const [isEditing, setIsEditing] = useState(false);
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(`http://localhost:3000/users/${userIdData}`);
+      console.log(res);
+      setUser(res.data);
+      setUserPosts(res.data.posts);
+    } catch (err) {
+      console.log(`Error while Fetching user Details : ${err}`);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          `http://localhost:3000/users/${userIdData}`
-        );
-        console.log(res);
-        setUser(res.data);
-        setUserPosts(res.data.posts);
-      } catch (err) {
-        console.log(`Error while Fetching user Details : ${err}`);
-      }
-    };
     if (userIdData) fetchData();
   }, [userIdData]);
-  const handleDeletePost = (post) => {
+  const handleDeletePost = async (post) => {
     console.log(`PostId of the post to be deleted : `, post.id);
     console.log(`public_id of the post to be deleted : `, post);
+    const updatedPosts = userPosts.filter((p) => p.id !== post.id);
+    await axios.patch(`http://localhost:3000/users/${userIdData}`, {
+      posts: [...updatedPosts],
+    });
+    fetchData();
+    console.log(`Post with postId ${post.id} deleted.`);
   };
   return (
     <div className={Styles.homePageContainer}>
@@ -31,32 +35,44 @@ const HomePage = ({ userIdData }) => {
       {userPosts &&
         userPosts.map((p) => (
           <div className={Styles.homePageContainer_postBox} key={p?.id}>
-            {p?.postPic
-              .match(/\/duidqdysu\/(\w+)\/upload/i)[1]
-              .toLowerCase() === "image" && (
-              <img
-                className={Styles.homePageContainer_postBox_postImg}
-                src={p?.postPic}
-                alt={`${p?.id} profilepic`}
-              />
+            {!isEditing &&
+              p?.postPic
+                .match(/\/duidqdysu\/(\w+)\/upload/i)[1]
+                .toLowerCase() === "image" && (
+                <img
+                  className={Styles.homePageContainer_postBox_postImg}
+                  src={p?.postPic}
+                  alt={`${p?.id} profilepic`}
+                />
+              )}
+            {!isEditing &&
+              p?.postPic
+                .match(/\/duidqdysu\/(\w+)\/upload/i)[1]
+                .toLowerCase() === "video" && (
+                <video
+                  className={Styles.homePageContainer_postBox_postImg}
+                  src={p?.postPic}
+                  alt={`${p?.id} profilepic`}
+                  autoPlay
+                  muted
+                  controls
+                />
+              )}
+            {isEditing && (
+              <p className={Styles.homePageContainer_postBox_postText}>
+                {p?.text}
+              </p>
             )}
-            {p?.postPic
-              .match(/\/duidqdysu\/(\w+)\/upload/i)[1]
-              .toLowerCase() === "video" && (
-              <video
-                className={Styles.homePageContainer_postBox_postImg}
-                src={p?.postPic}
-                alt={`${p?.id} profilepic`}
-                autoPlay
-                muted
-                controls
-              />
+            {!isEditing && (
+              <>
+                <button
+                  onClick={() => handleDeletePost(p)}
+                  className={Styles.deletePostBtn}
+                >
+                  Delete post
+                </button>
+              </>
             )}
-            <p className={Styles.homePageContainer_postBox_postText}>
-              {p?.text}
-            </p>
-            <button>Edit post</button>
-            <button onClick={() => handleDeletePost(p)}>Delete post</button>
           </div>
         ))}
     </div>
